b8de83910551c945086cd677299cfd667e6a1a8f
WeRockStar <kotchaphan.m@gmail.com>
2023-10-28 23:27:52 +0700

    Add README for chapter 9

--- chapter-09-starter/README.md
+++ chapter-09-starter/README.md
@@ -3,3 +3,11 @@
 ## Step 0: Create `NoteTest.java`
 
 ## Step 1: Try to test the code `Note.java`
+
+## Step 2: Understand dependency
+- `File System`
+- `DateTime`
+
+## Step 3: Apply dependency injection
+- Avoid hard `TextFile` on `Note`
+- Make birthday testable
---
+++ chapter-09-starter/src/test/java/NoteTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+    @Test
+    @DisplayName("given today is my birthday write reading note and my note should contain 🎂")
+    void writeNoteOnMyBirthday() {
+
+    }
+}
