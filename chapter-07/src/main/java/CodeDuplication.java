1f7a6b4f574cadb583d973076af6f3c1490558c4
WeRockStar <kotchaphan.m@gmail.com>
2023-10-28 22:16:19 +0700

    Add code duplicate and data clump example

--- chapter-07/src/main/java/CodeDuplication.java
+++ chapter-07/src/main/java/CodeDuplication.java
@@ -1,2 +1,49 @@
 public class CodeDuplication {
+    public void create() {
+        Product iPhone = new Product();
+        iPhone.setName("iPhone 15");
+        iPhone.setPrice(10000);
+
+        Product book = new Product();
+        book.setName("Clean Code");
+        book.setPrice(500);
+
+        Product apple = new Product();
+        apple.setName("Apple");
+        apple.setPrice(100);
+    }
+
+    public void refactored() {
+        Product iPhone = Product.create("iPhone 15", 10000);
+        Product book = Product.create("Clean Code", 500);
+        Product apple = Product.create("Apple", 50);
+    }
+}
+
+class Product {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    static Product create(String name, int price) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        return product;
+    }
 }
--- chapter-07/src/main/java/DataClump.java
+++ chapter-07/src/main/java/DataClump.java
@@ -1,2 +1,26 @@
+import java.time.LocalDate;
+
 public class DataClump {
+    void invoiceIn(LocalDate start, LocalDate end) {
+    }
+
+    void receivedIn(LocalDate start, LocalDate end) {
+    }
+
+    void overdueIn(LocalDate start, LocalDate end) {
+    }
+}
+
+class Refactored {
+    void invoiceIn(DateRange range) {
+    }
+
+    void receivedIn(DateRange range) {
+    }
+
+    void overdueIn(DateRange range) {
+    }
+}
+
+record DateRange(LocalDate start, LocalDate end) {
 }
