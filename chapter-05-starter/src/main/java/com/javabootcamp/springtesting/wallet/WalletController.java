0f58c0e9a110bbbb269032b5efa762700b9b23aa
WeRockStar <kotchaphan.m@gmail.com>
2023-10-29 09:45:23 +0700

    Remove unused import

--- chapter-05-starter/src/main/java/com/javabootcamp/springtesting/wallet/WalletController.java
+++ chapter-05-starter/src/main/java/com/javabootcamp/springtesting/wallet/WalletController.java
@@ -1,8 +1,6 @@
 package com.javabootcamp.springtesting.wallet;
 
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
