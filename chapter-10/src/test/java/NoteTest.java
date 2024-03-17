c31b6c4d52afe5496c6792b6730143b8539834cd
WeRockStar <kotchaphan.m@gmail.com>
2024-03-17 21:27:01 +0700

    Spotless apply

--- chapter-10/src/test/java/NoteTest.java
+++ chapter-10/src/test/java/NoteTest.java
@@ -8,84 +8,84 @@ import static org.mockito.Mockito.*;
 
 class NoteTest {
 
-    @Test
-    @DisplayName("given initial note, my note should be empty")
-    void initialNoteShouldBeEmpty() {
-        TextFile textFile = mock(TextFile.class);
-        when(textFile.read(any())).thenReturn("");
-        Note note = new Note(textFile);
+	@Test
+	@DisplayName("given initial note, my note should be empty")
+	void initialNoteShouldBeEmpty() {
+		TextFile textFile = mock(TextFile.class);
+		when(textFile.read(any())).thenReturn("");
+		Note note = new Note(textFile);
 
-        String expected = "";
-        String actual = note.read();
-        assertEquals(expected, actual);
-    }
+		String expected = "";
+		String actual = note.read();
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    @DisplayName("create reading book note, my note should return reading book")
-    void createReadingNote() {
-        TextFile textFile = mock(TextFile.class);
-        when(textFile.read(any())).thenReturn("Reading book");
-        Note note = new Note(textFile);
+	@Test
+	@DisplayName("create reading book note, my note should return reading book")
+	void createReadingNote() {
+		TextFile textFile = mock(TextFile.class);
+		when(textFile.read(any())).thenReturn("Reading book");
+		Note note = new Note(textFile);
 
-        note.write("Reading book");
+		note.write("Reading book");
 
-        String expected = "Reading book";
-        assertEquals(expected, note.read());
-    }
+		String expected = "Reading book";
+		assertEquals(expected, note.read());
+	}
 
-    @Test
-    @DisplayName("my default note file should be note.txt")
-    void defaultNoteFile() {
-        TextFile textFile = mock(TextFile.class);
-        Note note = new Note(textFile);
+	@Test
+	@DisplayName("my default note file should be note.txt")
+	void defaultNoteFile() {
+		TextFile textFile = mock(TextFile.class);
+		Note note = new Note(textFile);
 
-        String expected = "note.txt";
-        verify(textFile).create(expected);
-    }
+		String expected = "note.txt";
+		verify(textFile).create(expected);
+	}
 
-    @Test
-    @DisplayName("initial note should create note.txt only one file")
-    void createNoteFileOnlyOneTime() {
-        TextFile textFile = mock(TextFile.class);
-        Note note = new Note(textFile);
+	@Test
+	@DisplayName("initial note should create note.txt only one file")
+	void createNoteFileOnlyOneTime() {
+		TextFile textFile = mock(TextFile.class);
+		Note note = new Note(textFile);
 
-        int expected = 1;
-        verify(textFile, times(1)).create(any());
-    }
+		int expected = 1;
+		verify(textFile, times(1)).create(any());
+	}
 
-    @Test
-    @DisplayName("write reading note should call write method")
-    void writeNote() {
-        TextFile textFile = mock(TextFile.class);
-        Note note = new Note(textFile);
+	@Test
+	@DisplayName("write reading note should call write method")
+	void writeNote() {
+		TextFile textFile = mock(TextFile.class);
+		Note note = new Note(textFile);
 
-        note.write("Reading book");
+		note.write("Reading book");
 
-        verify(textFile).write("note.txt", "Reading book");
-    }
+		verify(textFile).write("note.txt", "Reading book");
+	}
 
-    @Test
-    @DisplayName("given today is my birthday write reading note and my note should contain 🎂")
-    void writeNoteOnMyBirthday() {
-        TextFile textFile = mock(TextFile.class);
-        BirthdayChecker birthdayChecker = mock(BirthdayChecker.class);
-        when(birthdayChecker.isBirthday()).thenReturn(true);
-        Note note = new Note(textFile, birthdayChecker);
+	@Test
+	@DisplayName("given today is my birthday write reading note and my note should contain 🎂")
+	void writeNoteOnMyBirthday() {
+		TextFile textFile = mock(TextFile.class);
+		BirthdayChecker birthdayChecker = mock(BirthdayChecker.class);
+		when(birthdayChecker.isBirthday()).thenReturn(true);
+		Note note = new Note(textFile, birthdayChecker);
 
-        note.write("Reading book");
+		note.write("Reading book");
 
-        String expected = "Reading book 🎂";
-        verify(textFile).write("note.txt", expected);
-    }
+		String expected = "Reading book 🎂";
+		verify(textFile).write("note.txt", expected);
+	}
 
-    @Test
-    @DisplayName("given write reading note async should success")
-    void writeAsyncNoteShouldSuccess() {
-        TextFile textFile = mock(TextFile.class);
-        BirthdayChecker birthdayChecker = mock(BirthdayChecker.class);
-        when(birthdayChecker.isBirthday()).thenReturn(true);
-        Note note = new Note(textFile, birthdayChecker);
+	@Test
+	@DisplayName("given write reading note async should success")
+	void writeAsyncNoteShouldSuccess() {
+		TextFile textFile = mock(TextFile.class);
+		BirthdayChecker birthdayChecker = mock(BirthdayChecker.class);
+		when(birthdayChecker.isBirthday()).thenReturn(true);
+		Note note = new Note(textFile, birthdayChecker);
 
-        note.writeAsync("Reading book", Schedulers.trampoline()).test().assertComplete();
-    }
+		note.writeAsync("Reading book", Schedulers.trampoline()).test().assertComplete();
+	}
 }
